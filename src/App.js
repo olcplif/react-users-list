762e72e52b94abf1ef9f575a4dd5a0dbe0246c7b
Oleksandr Lysenko <olysenko@softserveinc.com>
2023-04-28 15:54:25 +0300

    Use React Fragment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
-import Wrapper from "./components/Helpers/Wrapper";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
@@ -16,10 +15,10 @@ function App() {
     });
   };
   return (
-    <Wrapper>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </Wrapper>
+    </Fragment>
   );
 }
 
